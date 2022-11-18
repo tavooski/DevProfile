9446ec5fbb91e436b100cbd3d4c0c695a4c7782d
Gustavo <ruiz.gustavo97@gmail.com>
2022-11-17 18:24:09 -0800

    Initial commit

---
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.devprofile
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        val logo = findViewById<ImageView>(R.id.logo)
+        logo.setImageDrawable(rounded)
+    }
+}
